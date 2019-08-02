f6030cf748f32a0090eab5db43b41ef0be78748e
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-02 04:29:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   count = 0
   while count < array.length do
-    array.index(value_to_find)
+    array.find_index(value_to_find)
     count += 1
     return array
   end
